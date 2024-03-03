6b376a45417e737b5f9c149547bc48f2cdced7d3
dylanyang <yl976931569@163.com>
2024-03-03 21:56:43 +0800

    init

--- common/util.ts
+++ common/util.ts
@@ -1,3 +1,20 @@
+import { ethers, BigNumber, BigNumberish } from 'ethers'
+import {
+    EntryPoint, EntryPoint__factory,
+    UserOperationStruct
+} from '@account-abstraction/contracts'
+import {BaseAccountAPI} from "@account-abstraction/sdk/dist/src/BaseAccountAPI";
+import {SimpleAccountAPI} from "@account-abstraction/sdk/src/SimpleAccountAPI";
+
 export function buildCallData() {
     return "callDataBuider";
+}
+export function buildUserOp() {
+    // let  api = new SimpleAccountAPI({
+    //     index: 0,
+    //     owner: new ethers.Wallet(""),
+    //     factoryAddress: ""
+    // });
+
+    return "callDataBuider2";
 }
