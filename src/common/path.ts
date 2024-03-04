6a72c5d3f99869c5ace7354a916eb5155bffb0bf
dylan <yl976931569@163.com>
2024-03-04 14:08:18 +0800

    add basic recall function

---
+++ src/common/path.ts
@@ -0,0 +1,7 @@
+ export enum Path {
+    Health = "health",
+    GetSupportEntryPointV1 = "api/v1/get-support-entrypoint",
+    GetSupportStrategyV1 = "api/v1/get-support-strategy",
+    TryPayUserOperationV1 = "api/v1/try-pay-user-operation"
+}
+
--- src/ethPaymasterClient.ts
+++ src/ethPaymasterClient.ts
@@ -1,13 +1,66 @@
+import {BaseRequest, BaseResponse, ENV, Method} from "./common/type";
+import {generateUrl} from "./common/PaymasterUtil";
+import {Path} from "./common/path";
+
+export const DefaultProdHost = "https://EthPaymaster.org";
+export const DefaultDevHost = "https://dev.EthPaymaster.org";
+
 export class EthPaymasterClient {
-    static production(fetcher = fetch) {
-        return new EthPaymasterClient('https://EthPaymaster.org')
+
+    private readonly baseURL: string
+    private readonly fetch: typeof fetch
+    private readonly env: ENV
+
+    static production(baseUrl?: string, fetcher = fetch) {
+        if (baseUrl == undefined || baseUrl == '') {
+            baseUrl = DefaultProdHost
+        }
+        return new EthPaymasterClient(baseUrl, fetcher, ENV.PROD)
     }
 
-    static development(fetcher = fetch) {
-        return new EthPaymasterClient('https://pre.EthPaymaster.org')
+    static development(baseUrl?: string, fetcher = fetch) {
+        if (baseUrl == undefined || baseUrl == '') {
+            baseUrl = DefaultDevHost
+        }
+        return new EthPaymasterClient(baseUrl, fetcher, ENV.DEV)
     }
 
 
-    constructor(baseURL: string) {
+    constructor(baseURL: string, fetcher: typeof fetch, env: ENV) {
+        this.baseURL = baseURL
+        this.fetch = fetcher
+        this.env = env
     }
+
+    health(): Promise<BaseResponse> {
+        return this.request(Path.Health, Method.GET)
+    }
+
+    getSupportEntryPointV1(): Promise<BaseResponse> {
+        return this.request(Path.GetSupportEntryPointV1, Method.GET)
+    }
+
+    getSupportStrategyV1(): Promise<BaseResponse> {
+        return this.request(Path.GetSupportStrategyV1, Method.GET)
+    }
+
+    tryPayUserOperationV1(): Promise<BaseResponse> {
+        return this.request(Path.TryPayUserOperationV1, Method.POST)
+    }
+
+
+    protected async request<Request extends BaseRequest, Response>(path: Path, method: Method, body?: Request, urlParams?: Map<string, string>) {
+        let url = generateUrl(this.baseURL, path, urlParams)
+
+        let response = await this.fetch(url, {
+            method: method,
+            body: JSON.stringify(body)
+        })
+        if(!response.ok){
+            throw new Error(`Request failed with status ${response.status}`)
+        }
+        return await response.json() as Promise<Response>
+    }
+
 }
+
