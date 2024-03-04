b1279f4ea0f315aea557186a85be90c15e3d3619
dylan <yl976931569@163.com>
2024-03-04 09:35:04 +0800

    update

--- src/ethPaymasterClient.ts
+++ src/ethPaymasterClient.ts
@@ -1,4 +1,13 @@
 export class EthPaymasterClient {
-    constructor() {
+    static production(fetcher = fetch) {
+        return new EthPaymasterClient('https://EthPaymaster.org')
+    }
+
+    static development(fetcher = fetch) {
+        return new EthPaymasterClient('https://pre.EthPaymaster.org')
+    }
+
+
+    constructor(baseURL: string) {
     }
 }
